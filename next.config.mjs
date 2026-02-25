e19017bd1ecb8cdcc24b7a7ad4da6948a7702fa9
IslamElHelou <islamelhelou@gmail.com>
2026-02-25 08:24:44 +0200

    update project

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp']
   },
+  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.1.8:3000'],
   async headers() {
     return [
       {
